74b784dd8dc9ff723e61fa6457472f5311642941
Vladislav Safonichev <unsviperm@gmail.com>
2019-06-11 23:28:39 +0300

    Fix fav button animation on load

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // https://stackoverflow.com/questions/4880381/check-whether-html-element-has-scrollbars
+
+const favDoctorsBtns = document.querySelectorAll('.fav-doc-btn');
+
+favDoctorsBtns.forEach(btn => {
+    if (btn.children[0].classList.contains('fav-btn-load')) {
+        btn.children[0].addEventListener('click', function () {
+            btn.children[0].classList.remove('fav-btn-load');
+            btn.children[0].classList.add('fav-btn');
+        });
+    };
+});
+
+// TODO: Change bell on click
 // TODO: 
 // TODO: 
 // TODO: 
