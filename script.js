6219df711bc99a9c0e20fd7ef1cc25afefbf32ed
Vladislav Safonichev <unsviperm@gmail.com>
2019-06-12 23:52:56 +0300

    Add modal window for upload json. Comment code.

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-// https://stackoverflow.com/questions/4880381/check-whether-html-element-has-scrollbars
+// ##############################################
+// ################ MODAL WINDOW ################
+// ##############################################
 
+// DOMs
+const uploadDoctorBtn = document.querySelector('#upload_doctors');
+const modalWindow = document.querySelector('.modal');
+const modalMessage = document.querySelector('.modal-message');
+const closeModalBtn = document.querySelector('.close-modal');
+
+// Variables
+let isModalClosed = true;
+
+// Functions
+function showModal() {
+    modalWindow.classList.add('show');
+    modalWindow.classList.add('fade');
+};
+
+function closeModal() {
+    modalWindow.classList.remove('fade');
+};
+
+// Events
+uploadDoctorBtn.addEventListener('click', showModal);
+
+modalWindow.addEventListener('transitionend', function (e) {
+    if (e.target === modalWindow) {
+        isModalClosed = !isModalClosed;
+        if (isModalClosed) {
+            modalWindow.classList.remove('show');
+        };
+    };
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+window.addEventListener('click', function (e) {
+    if ((!isModalClosed) && (e.target !== modalMessage) && (!modalMessage.contains(e.target))) {
+        closeModal();
+    };
+});
+
+// ##############################################
+// ############ OBJECTS MANIPULATION ############
+// ##############################################
+
+// DOMs
 const favDoctorsBtns = document.querySelectorAll('.fav-doc-btn');
+const uploadSampleBtn = document.querySelector('#upload_sample');
+const uploadFromPCBtn = document.querySelector('#upload_from_pc');
 
+// Events
 favDoctorsBtns.forEach(btn => {
     if (btn.children[0].classList.contains('fav-btn-load')) {
         btn.children[0].addEventListener('click', function () {
@@ -11,7 +59,8 @@ favDoctorsBtns.forEach(btn => {
     };
 });
 
+
+uploadFromPCBtn.addEventListener('click', () => document.getElementById('file_input').click());
+
 // TODO: Change bell on click
-// TODO: 
-// TODO: 
-// TODO: 
+// TODO: Add object manipulation
